50e6d29ee5b2dbe7e11ceb9c7c5e884dc80f6495
MaggieNush <maggienungari.mn@gmail.com>
2025-06-11 12:36:01 +0300

    Add event listeners to the script file

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
         feedbackDiv.style.display = "block";
 
         if(isValid) {
-            feedbackDiv.textContent = "Registration was successful!";
+            feedbackDiv.textContent = "Registration successful!";
             feedbackDiv.style.color = "#28a745"
         } else {
             feedbackDiv.innerHTML = messages.join('<br>')
